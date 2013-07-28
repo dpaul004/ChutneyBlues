42318f518289ec6cf7848a79324db5aacebc98b7
Ubuntu <ubuntu@ip-172-31-21-94.us-west-2.compute.internal>
2013-07-28 21:54:39 +0000

    web.js

---
+++ web.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var fs = require('fs');
+
+var input_file = 'ChutneyBlues.htm';
+
+var app = express.createServer(express.logger());
+
+var readfile = function(input_file) {
+    return fs.readFileSync(input_file).toString();
+
+};
+
+app.get('/', function(request, response) {
+  response.send(readfile(input_file));
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
